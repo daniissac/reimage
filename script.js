3e0cdb9e577eb71dbda91880ce8321197bbe013f
Dani Issac <daissac@thousandeyes.com>
2024-10-04 12:46:09 +0530

    PWA implementation

--- script.js
+++ script.js
@@ -216,3 +216,11 @@ function dataURLtoFile(dataurl, filename) {
 }
 
 toggleEditControls();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => console.log('ServiceWorker registered'))
+      .catch(error => console.log('ServiceWorker registration failed:', error));
+  });
+}
